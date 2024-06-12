53382d6a183214581df62b2af03a8a0eb8d5706f
JK <jenny.kaunistunturi@edunord.se>
2024-06-12 15:19:34 +0200

    Added some number tests

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Giraffe
+﻿using System.Numerics;
+
+namespace Giraffe
 {
     internal class Program
     {
@@ -6,15 +8,17 @@
         {
             Console.WriteLine("Hello, stranger, type your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine($"Hello, {name}!");
+            Console.WriteLine("Add a number: ");
+            string num = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}, you added number " + num + "!");
 
 
             string characterName = "Teppo the cat";
             int characterAge = 5;
             char letter = 'K';
-
             bool isAmazing = true;
 
+            Console.WriteLine(" ");
             Console.WriteLine("Here is a story: ");
             Console.WriteLine(characterName + " was a happy cat, and he was " + characterAge + " years young.");
             characterName = "Seppo the dog";
@@ -28,6 +32,19 @@
             Console.WriteLine("and what is the indexs of letter 'h'? " + favoritePhrase.IndexOf("h"));
 
             Console.WriteLine(favoritePhrase.Substring(5, 1));
+
+
+            //working with numbers
+
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3, 2));
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Max(2, 360));
+            Console.WriteLine(Math.Min(2, 360));
+            Console.WriteLine(Math.Round(5.8));
+
+            int number = Convert.ToInt32("45");
+            Console.WriteLine(number + 6);
         }
 
     }
